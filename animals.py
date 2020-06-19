c1acfc393714248d983cd453bcab91013c066119
Никита Зубков <nikitazubkov@MacBook-Pro-Nikita.local>
2020-06-19 16:28:45 +0400

    Cats ok

--- animals.py
+++ animals.py
@@ -1,5 +1,9 @@
-from flask import Flask
+from flask import Flask, send_file
+from PIL import Image, ImageFilter
+from io import BytesIO
+from pathlib import Path
 import requests
+import uuid
 
 app = Flask(__name__)
 
@@ -10,8 +14,20 @@ def helloWorld():
 
 @app.route('/animal/cat')
 def getAnimalCat():
-    r = requests.get("http://aws.random.cat/meow")
-    return r.json()['file']
+    url_req = requests.get("http://aws.random.cat/meow")
+    image_req = requests.get(url_req.json()['file'])
+    image = Image.open(BytesIO(image_req.content)).convert('RGB')
+    image = image.filter(ImageFilter.CONTOUR).filter(ImageFilter.SMOOTH)
+    name = uuid.uuid4()
+
+    Path("img").mkdir(parents=True, exist_ok=True)
+
+    filename = "img/" + str(name) + ".png"
+
+    image.save(filename)
+
+
+    return send_file(filename, mimetype='image/png')
 
 @app.route('/animal/dog')
 def getAnimalDog():
