6177d95e098aad0aae4afa523c70cf115046ed09
Никита Зубков <nikitazubkov@MacBook-Pro-Nikita.local>
2020-06-19 17:42:43 +0400

    refactor /animal/

--- animals.py
+++ animals.py
@@ -1,41 +1,62 @@
-from flask import Flask, send_file
+from flask import Flask, send_file, abort
 from PIL import Image, ImageFilter
 from io import BytesIO
 from pathlib import Path
 import requests
 import uuid
 
-app = Flask(__name__)
+
+
+def create_app():
+    #create path to store images
+    Path("img").mkdir(parents=True, exist_ok=True)
+    return Flask(__name__)
+    
+app = create_app()
+
+
+
+urls = {
+    'cat': lambda: requests.get("http://aws.random.cat/meow").json()['file'],
+    'dog': lambda: requests.get("http://shibe.online/api/shibes?count=1").json()[0],
+    'fox': lambda: requests.get("https://randomfox.ca/floof/").json()['image'],
+}
+
+uuid_generate = lambda: uuid.uuid4()
+
+def get_image(url):
+    r = requests.get(url)
+    image = Image.open(BytesIO(r.content)).convert('RGB')
+    (w,h) = (1024, round(image.height * (1024/image.width)))
+    image = image.resize((w,h))
+    return image
+
+def filter_image(image):
+    # image = image.filter(ImageFilter.CONTOUR)
+    image = image.filter(ImageFilter.SMOOTH_MORE)
+    return image
+
 
 @app.route('/')
 def helloWorld():
     return 'Hello world!'
 
 
-@app.route('/animal/cat')
-def getAnimalCat():
-    url_req = requests.get("http://aws.random.cat/meow")
-    image_req = requests.get(url_req.json()['file'])
-    image = Image.open(BytesIO(image_req.content)).convert('RGB')
-    image = image.filter(ImageFilter.CONTOUR).filter(ImageFilter.SMOOTH)
-    name = uuid.uuid4()
+@app.route('/animal/<animaltype>')
+def getAnimal(animaltype):
+    if((animaltype in urls) == False):
+        abort(404)
+    
+    image = get_image(urls[animaltype]())
+    image = filter_image(image)
 
-    Path("img").mkdir(parents=True, exist_ok=True)
-
-    filename = "img/" + str(name) + ".png"
+    filename = "img/" + str(uuid_generate()) + ".png"
 
     image.save(filename)
 
-
     return send_file(filename, mimetype='image/png')
 
-@app.route('/animal/dog')
-def getAnimalDog():
-    return 'this is dog \"(ᓄ ᴥ ᓇ)\\'
 
-@app.route('/animal/fox')
-def getAnimalFox():
-    return 'this is fox 🦊'
 
 @app.route('/history/')
 def history():
