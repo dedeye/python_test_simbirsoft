b3dcc3e956391f017511ec7a57c7d6ab7dfdef6e
Никита Зубков <nikitazubkov@MacBook-Pro-Nikita.local>
2020-07-02 16:54:31 +0400

    fix setup

--- setup.py
+++ setup.py
@@ -5,13 +5,9 @@ setup(
     name="animalfilter",
     version="0.0.1",
 
-
-    # maintainer="Pallets team",
-    # maintainer_email="contact@palletsprojects.com",
-    description="test assignment for simbirsoft",
+    description="test task for simbirsoft",
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
-    install_requires=["flask", "SQLAlchemy", "Pillow", "requests"],
-    extras_require={"test": ["pytest", "coverage"]},
+    install_requires=["Flask==1.1.2", "Flask-SQLAlchemy==2.4.3", "Pillow==7.1.2", "requests==2.24.0"],
 )
