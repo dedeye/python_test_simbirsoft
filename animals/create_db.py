41cf3290e07489a5c221963df5aad3ecb16966be
Никита Зубков <nikitazubkov@MacBook-Pro-Nikita.local>
2020-07-02 17:07:56 +0400

    setup fixes

---
+++ animals/create_db.py
@@ -0,0 +1,3 @@
+from animals import db
+
+db.create_all()
--- animals/runserver.py
+++ animals/runserver.py
@@ -1,8 +1,7 @@
 from animals import app
 from pathlib import Path
 
-
+# create img folder if not exists
 Path(app.config['IMG_FOLDER']).mkdir(parents=True, exist_ok=True)
 
-
-app.run(debug=True)
+app.run()
